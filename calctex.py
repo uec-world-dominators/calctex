59302a5aafd2e5b9d155c50071fcf24e35662fbe
sho <me@shosato.jp>
2019-12-01 15:42:24 +0900

    途中

--- calctex.py
+++ calctex.py
@@ -1,17 +1,90 @@
+import math
 import sympy as sym
 import sympy.physics.units as u
+import common
 u_m = sym.UnevaluatedExpr(u.m)
 u_s = sym.UnevaluatedExpr(u.s)
 u_kg = sym.UnevaluatedExpr(u.kg)
 a = ""
 b = ""
+
+o = {
+    'm': {'d': -1, 'e': -3},
+    's': {'d': 1, 'e': 1},
+}
+
+
+class Unit:
+    '''
+    基本的な単位・複雑な単位（Pa）
+    '''
+
+    def update_table(self, target):
+        for ak, av in self.table:
+            if ak in target:
+                self.table[ak] = {
+                    'd': av['d']+target[ak]['d'],
+                    'e': av['e']+target[ak]['e']
+                }
+
+    def __init__(self, table={}):
+        self.table
+
+    def __rmul__(self, e):
+        u = Unit(self.table.copy())
+        if isinstance(e, Unit):
+            u.update_table(e.table)
+            return u
+        else:
+            u.update_table({k: {'d': 0, 'e': math.log10(e)} for k, v in u.table})
+            return u
+
+    def __repr__(self):
+        return f"<{self.symbol} e={self.e}>"
+
+    def __pow__(self):
+        pass
+
+m = Unit('m')
+s = Unit('s')
+kg = Unit('kg')
+N = kg * m * s**-2
+Pa = N * m**-2
+mm = m * 10e-3
+nm = m * 10e-9
+print(m, mm, nm)
+
+
+class Value:
+    '''
+    数値と単位の演算
+    '''
+
+    def __init__(self, value, prec=6, units={}):
+        self.value = value
+        self.prec = prec
+        self.units = units
+        tex_units = ""
+        if units:
+            for i, j in units.item:
+                if j["d"] != 0 and j["d"] != 1:
+                    tex_units += str(i) + f"^{{{i["d"]}}}"
+                elif j["d"] == 0:
+                    tex_units += ""
+                elif j["d"] == 1:
+                    tex_units += str(i)
+        self.tex = common(value) + " \, " + tex_units
+    def __repr__(self):
+        return self.tex
+print(Value(10, 4, o))
+
+
 class Calc:
     def __init__(self, value, prec=6, tex="", parentheses=False):
         self.value = value
         self.prec = prec
         if tex:
-            self.tex = tex
-        else:
+            self.tex =
             self.tex = sym.latex(value)
         self.parentheses = parentheses
 
@@ -26,7 +99,6 @@ class Calc:
             tex = "%s + %s" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=True)
 
-
     def __sub__(self, other):
         if isinstance(other, Calc):
             value = self.value - other.value
@@ -37,7 +109,7 @@ class Calc:
             prec = self.prec
             tex = "%s - %s" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=True)
-    
+
     def __mul__(self, other):
         if isinstance(other, Calc):
             value = self.value * other.value
@@ -60,7 +132,7 @@ class Calc:
             else:
                 tex = r"%s \times %s" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=False)
-    
+
     def __truediv__(self, other):
         if isinstance(other, Calc):
             value = self.value / other.value
@@ -69,7 +141,7 @@ class Calc:
                 if other.parentheses:
                     tex = r"\frac{\left(%s\right)}{\left(%s\right)}" % (self.tex, other.tex)
                 else:
-                    tex =r"\frac{\left(%s\right)}{%s}" % (self.tex, other.tex)
+                    tex = r"\frac{\left(%s\right)}{%s}" % (self.tex, other.tex)
             else:
                 if other.parentheses:
                     tex = r"\frac{%s}{\left(%s\right)}" % (self.tex, other.tex)
@@ -83,7 +155,7 @@ class Calc:
             else:
                 tex = r"\frac{%s}{%s}" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=False)
-    
+
         if isinstance(other, Calc):
             value = self.value**other.value
             prec = min([self.prec, other.prec])
@@ -105,7 +177,7 @@ class Calc:
             else:
                 tex = r"{%s}^{%s}" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=False)
-    
+
     def __pow__(self, other):
         if isinstance(other, Calc):
             value = self.value**other.value
@@ -128,7 +200,6 @@ class Calc:
             else:
                 tex = r"{%s}^{%s}" % (self.tex, other)
         return Calc(value=value, prec=prec, tex=tex, parentheses=False)
-
 
     def __radd__(self, other):
         if isinstance(other, Calc):
@@ -183,7 +254,7 @@ class Calc:
                 if other.parentheses:
                     tex = r"\frac{\left(%s\right)}{\left(%s\right)}" % (other.tex, self.tex)
                 else:
-                    tex =r"\frac{\left(%s\right)}{%s}" % (other.tex, self.tex)
+                    tex = r"\frac{\left(%s\right)}{%s}" % (other.tex, self.tex)
             else:
                 if other.parentheses:
                     tex = r"\frac{%s}{\left(%s\right)}" % (other.tex, self.tex)
@@ -203,10 +274,15 @@ class Calc:
 
     def doit(self):
         return self.value.doit()
-    
+
     def latex(self, variable=""):
+        print(self.value)
         latex = r"""\begin{align*}
     %s &= %s \\
     &= %s
 \end{align*}""" % (variable, self.tex, sym.latex(self.value.doit()))
         print(latex)
+
+
+a = (Calc(20*u_m)+Calc(3*u_m)) * Calc(3)
+a.latex('N')
---
+++ common.py
@@ -0,0 +1,26 @@
+import math
+
+# よくわからないけど動く（さわるな危険）
+def roundtex(n, d, exp=False):
+    assert(d > 0)
+    if n == 0:
+        return f"0{('.'+'0'*(d-1)) if d!=1 else ''}"
+    elif n > 1:
+        _d = math.floor(math.log10(n))
+        s = str(round(n/10**_d, d-1))
+        return f'{s}{"0"*(1+d-len(s))}' + (f'\\times 10^{{{_d}}}' if _d != 0 else '')
+    else:
+        _n = n
+        _d = 0
+        while True:
+            _n *= 10
+            _d += 1
+            if math.floor(_n) % 10 != 0:
+                s = str(round(_n, d-1) if d != 1 else round(_n))
+                return f'{s}{"0"*(1+d-len(s)) if d!=1 else ""}' + (f'\\times 10^{{{-_d}}}' if _d != 0 else '')
+
+
+isinpackage = not __name__ in ['common', '__main__']
+if not isinpackage:
+    assert(roundtex(0.99366103, 4) == r'9.937\times 10^{-1}')
+    print('OK')
