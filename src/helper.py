592214dcf9a31e88ae073a6e7d8497c9be68138c
sho <me@shosato.jp>
2019-12-11 21:23:29 +0900

    helper関数の実装

---
+++ src/helper.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+from .value import Value
+
+def point_digit_to_significant_figure(n, point_digit=0):
+    return math.ceil(math.log10(n * 10 ** -point_digit))
+
+
+def decimal_point(data, point_digit, unit):
+    '''
+    data : 数字のリスト
+    point_digit : その桁数以上の数字を有効数字とする（小数点第一位は-1）
+    unit : Unitオブジェクト
+    '''
+    return np.array(list(map(lambda n:
+                             Value(n, unit.clone(), point_digit_to_significant_figure(n, point_digit)), data)))
