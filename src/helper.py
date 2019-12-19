baf132f9eedd3a7d3b7623057124f314cf527910
shosatojp <me@shosato.jp>
2019-12-19 22:03:35 +0900

    引数の順番を変更

--- src/helper.py
+++ src/helper.py
@@ -8,7 +8,7 @@ def point_digit_to_significant_figure(n, point_digit=0):
     return math.ceil(math.log10(n * 10 ** -point_digit))
 
 
-def decimal_point(data, point_digit, unit=Unit({})):
+def decimal_point(data, unit=Unit({}), point_digit=0):
     '''
     data : 数字のリスト
     point_digit : その桁数以上の数字を有効数字とする（小数点第一位は-1）
@@ -18,5 +18,5 @@ def decimal_point(data, point_digit, unit=Unit({})):
                              Value(n, unit and unit.clone(), point_digit_to_significant_figure(n, point_digit)), data)))
 
 
-def multi(data, significant, unit=Unit({})):
+def multi(data, unit=Unit({}), significant=math.inf):
     return np.array(list(map(lambda e: Value(e, unit and unit.clone(), significant), data)))
