44c97bf26b4d3c13b5a99d4e1243cc9d7aed15fa
shosatojp <me@shosato.jp>
2019-12-19 21:57:29 +0900

    デフォルト引数を設定

--- src/helper.py
+++ src/helper.py
@@ -1,21 +1,22 @@
 import numpy as np
 import math
 from src.value import Value
+from src.unit import Unit
 
 
 def point_digit_to_significant_figure(n, point_digit=0):
     return math.ceil(math.log10(n * 10 ** -point_digit))
 
 
-def decimal_point(data, point_digit, unit):
+def decimal_point(data, point_digit, unit=Unit({})):
     '''
     data : 数字のリスト
     point_digit : その桁数以上の数字を有効数字とする（小数点第一位は-1）
     unit : Unitオブジェクト
     '''
     return np.array(list(map(lambda n:
-                             Value(n, unit.clone(), point_digit_to_significant_figure(n, point_digit)), data)))
+                             Value(n, unit and unit.clone(), point_digit_to_significant_figure(n, point_digit)), data)))
 
 
-def multi(data, significant, unit=None):
+def multi(data, significant, unit=Unit({})):
     return np.array(list(map(lambda e: Value(e, unit and unit.clone(), significant), data)))
