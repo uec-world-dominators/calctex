f2fe3f308fb432019e91b5042e92b5d2d49b565c
shosatojp <me@shosato.jp>
2019-12-20 09:58:33 +0900

    整理

--- setup.py
+++ setup.py
@@ -8,29 +8,22 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='calctex',  # パッケージ名(プロジェクト名)
-    packages=['calctex'],  # パッケージ内(プロジェクト内)のパッケージ名をリスト形式で指定
-
-    version='0.0.1',  # バージョン
-
-    license='MIT',  # ライセンス
-
-    install_requires=['numpy'],  # pip installする際に同時にインストールされるパッケージ名をリスト形式で指定
-
-    author='shosatojp',  # パッケージ作者の名前
-    author_email='me@shosato.jp',  # パッケージ作者の連絡先メールアドレス
-
-    url='https://github.com/uec-world-dominators/calctex',  # パッケージに関連するサイトのURL(GitHubなど)
-
-    description='Show latex formula of calclation',  # パッケージの簡単な説明
-    long_description=long_description,  # PyPIに'Project description'として表示されるパッケージの説明文
-    long_description_content_type='text/markdown',  # long_descriptionの形式を'text/plain', 'text/x-rst', 'text/markdown'のいずれかから指定
-    keywords='calctex calc-tex calc tex latex',  # PyPIでの検索用キーワードをスペース区切りで指定
-
+    name='calctex',
+    packages=['calctex'],
+    version='0.0.1',
+    license='MIT',
+    install_requires=['numpy'],
+    author='shosatojp',
+    author_email='me@shosato.jp',
+    url='https://github.com/uec-world-dominators/calctex',
+    description='Show latex formula of calclation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='calctex calc-tex calc tex latex',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-    ],  # パッケージ(プロジェクト)の分類。https://pypi.org/classifiers/に掲載されているものを指定可能。
+    ],
 )
